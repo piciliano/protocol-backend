ef5629c9609af5ccb5e864ec3ae462fed8ff80e2
neto <picilianovasconcelos@gmail.com>
2025-06-03 13:03:03 -0300

    refactor(auth): use ConfigService to retrieve JWT secret key

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,19 +1,24 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { PrismaService } from 'prisma/prisma.service';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private prisma: PrismaService,
-    configService: ConfigService,
-  ) {
+  constructor(private readonly prisma: PrismaService) {
+    const secret = process.env.SECRET_OR_KEY;
+    if (!secret) {
+      throw new InternalServerErrorException('JWT secret key is not defined');
+    }
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.getOrThrow<string>('SECRET_OR_KEY'),
+      secretOrKey: secret,
     });
   }
 
