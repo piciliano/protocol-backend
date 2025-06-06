f64976c48f096dc50d397067f08f1b2dd039100e
neto <picilianovasconcelos@gmail.com>
2025-06-06 16:52:02 -0300

    refactor: geocode debug

--- src/geocode/geocode.controller.ts
+++ src/geocode/geocode.controller.ts
@@ -13,11 +13,21 @@ export class GeocodeController {
   async getCoordinates(@Query('q') address: string) {
     if (!address) return [];
 
-    const url = `https://geocode.maps.co/search?format=json&q=${encodeURIComponent(address)}`;
+    const apiKey = process.env.API_KEY;
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${apiKey}&language=pt&countrycode=br&limit=1`;
+
     try {
       const response = await axios.get(url);
+      const result = response.data.results[0];
 
-      return response.data;
+      if (!result) return [];
+
+      return {
+        lat: result.geometry.lat,
+        lng: result.geometry.lng,
+        components: result.components,
+        formatted: result.formatted,
+      };
     } catch (error) {
       console.error('Erro ao buscar coordenadas:', error.message);
       return [];
