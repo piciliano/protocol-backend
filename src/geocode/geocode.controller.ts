b86d9a9d807280da8bacc36bf2597a680d3ded68
neto <picilianovasconcelos@gmail.com>
2025-06-06 15:41:41 -0300

    refactor: debug geocode

--- src/geocode/geocode.controller.ts
+++ src/geocode/geocode.controller.ts
@@ -11,34 +11,15 @@ export class GeocodeController {
   @Get()
   @SwaggerGetCoordinates()
   async getCoordinates(@Query('q') address: string) {
-    console.log('Recebido no backend o endereço:', address);
-
-    if (!address) {
-      console.log('Nenhum endereço fornecido, retornando array vazio.');
-      return [];
-    }
+    if (!address) return [];
 
     const url = `https://geocode.maps.co/search?format=json&q=${encodeURIComponent(address)}`;
-    console.log('URL formada para consulta:', url);
-
     try {
       const response = await axios.get(url);
 
-      if (!response.data || response.data.length === 0) {
-        console.log('Resposta vazia recebida do Nominatim.');
-      }
-
       return response.data;
     } catch (error) {
-      console.error('Erro ao buscar coordenadas no Nominatim:', error.message);
-
-      if (error.response) {
-        console.error('Status do erro:', error.response.status);
-        console.error('Dados do erro:', error.response.data);
-      } else {
-        console.error('Erro sem resposta HTTP:', error);
-      }
-
+      console.error('Erro ao buscar coordenadas:', error.message);
       return [];
     }
   }
